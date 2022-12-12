3a42edac9bd1cb5bb831e53335790b18807f7e6d
Kevin Santiago <113930456+ksantiago772@users.noreply.github.com>
2022-12-12 18:59:56 +0000

    kk

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ list = w_list()
 # print("Hello, we are playing a game of hangman. Im thinking of a word and you have to guess it.")
 
 word = random.choice(list)
-play(word)   
-word_letters = word.split 
+# print(word)
+p = play(word)   
+print(p)
+word_letters = word.split()
+
 print(word_letters)
 
  
-
+print("Thats the end of the game!!")
   
--- playing.py
+++ playing.py
@@ -1,20 +1,22 @@
 def play(word):
- word_completion = "_" * len(word)
+ word_completion = "_ " * len(word)
  guessed = False
  guessed_letters = []
+ approved_letters = []
  guessed_word = []
  num_lives = 6
-
+ # tries = 0
  print("Hello, we are playing a game of hangman. Im thinking of a word and you have to guess it.")
  print(word_completion)
 
+   
 
 
-
- while not guessed and num_lives > 0:
+ while not guessed and num_lives > 0 :
     guess = input("What letter would you like to guess?").lower()
+    
     if guess in guessed_letters:
-        print("you already guessed the letter")
+        print("You already guessed the letter, try again")
     if str(guess) not in word:
         print(guess, "is not in the word")
         num_lives -= 1 
@@ -23,8 +25,23 @@ def play(word):
 
     if guess in word:
         print("you got a letter")
-        guessed_letters.append(guess)
+        approved_letters.append(guess)
+        if approved_letters ==  word:
+          print("You Win!")
+          we_stole_this = [letter if letter in approved_letters else '-' for letter in word]
+      print('current word:', ' '.join(we_stole_this))
+           break
+    if guess not in  word:
+      guessed_letters.append(guess)
+    
        
+    
+      
+      
     else:("not a valid guess")
     print(word_completion)
- return play
+
+
+ 
+
+
